15da436287bc4f1d8f08bbdbf54b28150a8d54fc
Marta Probierz <75137091+marta-probierz@users.noreply.github.com>
2022-03-04 23:55:34 +0100

    Create Length of tuple.ts

---
+++ Length of tuple.ts
@@ -0,0 +1,27 @@
+/**
+ * For given a tuple, you need create a generic Length
+ * that gets the length of the tuple.
+ */
+
+type Length<T extends { length: number }> = T["length"];
+
+
+////////////////////////////////////////////////////////////////////////////////
+////////////////////   TEST CASES - look but don't modify   ////////////////////
+////////////////////////////////////////////////////////////////////////////////   
+const tesla = ['tesla', 'model 3', 'model X', 'model Y'] as const
+const spaceX = ['FALCON 9', 'FALCON HEAVY', 'DRAGON', 'STARSHIP', 'HUMAN SPACEFLIGHT'] as const
+
+type cases = [
+  Expect<Equal<Length<typeof tesla>, 4>>,
+  Expect<Equal<Length<typeof spaceX>, 5>>,
+  // @ts-expect-error
+  Length<5>,
+  // @ts-expect-error
+  Length<'hello world'>,
+]
+
+type Expect<T extends true> = T
+type Equal<X, Y> =
+  (<T>() => T extends X ? 1 : 2) extends
+  (<T>() => T extends Y ? 1 : 2) ? true : false
