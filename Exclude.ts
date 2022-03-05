f43d37b793aa3a91452c8d566a3f0589028eed5a
Marta Probierz <75137091+marta-probierz@users.noreply.github.com>
2022-03-05 13:33:45 +0100

    Create Exclude.ts

---
+++ Exclude.ts
@@ -0,0 +1,20 @@
+/**
+ * Implement the built-in Exclude<T, U> that excludes
+ * from T those types that are assignable to U.
+ */
+
+type MyExclude<T, U> = Exclude<T, U>
+
+////////////////////////////////////////////////////////////////////////////////
+////////////////////   TEST CASES - look but don't modify   ////////////////////
+////////////////////////////////////////////////////////////////////////////////   
+type cases = [
+    Expect<Equal<MyExclude<"a" | "b" | "c", "a">, Exclude<"a" | "b" | "c", "a">>>,
+    Expect<Equal<MyExclude<"a" | "b" | "c", "a" | "b">, Exclude<"a" | "b" | "c", "a" | "b">>>,
+    Expect<Equal<MyExclude<string | number | (() => void), Function>, Exclude<string | number | (() => void), Function>>>,
+]
+
+type Expect<T extends true> = T
+type Equal<X, Y> =
+  (<T>() => T extends X ? 1 : 2) extends
+  (<T>() => T extends Y ? 1 : 2) ? true : false
