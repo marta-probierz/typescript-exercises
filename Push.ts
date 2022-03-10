975d1c50dfec04b753b927ea432072a549c6604a
Marta Probierz <75137091+marta-probierz@users.noreply.github.com>
2022-03-10 10:57:58 +0100

    Create Push.ts

---
+++ Push.ts
@@ -0,0 +1,19 @@
+/**
+ * Implement the generic version of Array.push
+ */
+
+type Push<T extends unknown[], U> = [...T, U];
+
+////////////////////////////////////////////////////////////////////////////////
+////////////////////   TEST CASES - look but don't modify   ////////////////////
+////////////////////////////////////////////////////////////////////////////////   
+type cases = [
+  Expect<Equal<Push<[], 1>, [1]>>,
+  Expect<Equal<Push<[1, 2], '3'>, [1, 2, '3']>>,
+  Expect<Equal<Push<['1', 2, '3'], boolean>, ['1', 2, '3', boolean]>>,
+]
+
+type Expect<T extends true> = T
+type Equal<X, Y> =
+  (<T>() => T extends X ? 1 : 2) extends
+  (<T>() => T extends Y ? 1 : 2) ? true : false
